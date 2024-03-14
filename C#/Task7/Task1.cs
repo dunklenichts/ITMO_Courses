24f51880910bcf7ab279d67fa05560e449cf1752
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-14 21:03:38 +0300

    Add files via upload

---
+++ C#/Task7/Task1.cs
@@ -0,0 +1,51 @@
+﻿/* Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше 
+ * (создать метод для вычисления  площади  треугольника по длинам его сторон).  Для решения 
+ * задачи можно использовать формулу Герона */
+
+namespace Task7
+{
+    class Task1
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+
+            return  Math.Sqrt(p * (p - a)*(p - b)*(p - c));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a1:");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b1:");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter c1:");
+            double c1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a2:");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b2:");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter c2:");
+            double c2 = Convert.ToDouble(Console.ReadLine());
+
+            double s1 = GetSquare(a1, b1, c1);
+            double s2 = GetSquare(a2, b2, c2);
+
+            if (s1 < s2)
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            else if (s1 > s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Task7/Task2.cs
@@ -0,0 +1,26 @@
+﻿/* Создать метод для вычисления объема и площади поверхности куба по длине его ребра. */
+
+namespace Task7
+{
+    class Task2
+    {
+        static void GetSquareVolume(int a, out double S, out double V)
+        {
+            V = Math.Pow(a, 3);
+            S = 6 * Math.Pow(a, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введине сторону куба: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S;
+            double V;
+
+            GetSquareVolume(a, out S, out V);
+            Console.WriteLine("Площадь поверхности: {0}", S);
+            Console.WriteLine("Объем куба: {0}", V);
+            Console.ReadKey();
+        }
+    }
+}
