8b148d12f40ff0f5137931358ff25cef1d2680a6
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-20 21:20:50 +0300

    Add files via upload

---
+++ C#/Task11/Task11/Func.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    struct Func
+    {
+        double k;
+        double b;
+
+        public Func(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            double x = -(double)b / k;
+
+            if (k < 0)
+            {
+                Console.Write("Функция убывает");
+            }
+            else if (k > 0)
+            {
+                Console.Write("Функция возрастает");
+            }
+            else
+            {
+                return "Функция параллельна Ох";
+            }
+
+            Console.WriteLine();
+
+            return $"x = {Math.Round(x, 3)}";
+        }
+    }
+}
---
+++ C#/Task11/Task11/structure.cs
@@ -0,0 +1,26 @@
+﻿/* Разработать структуру для решения линейного уравнения 0=kx+b. 
+ * Коэффициенты уравнения k, b реализовать с помощью полей вещественного 
+ * типа. Для решения уравнения предусмотреть метод Root. Создать экземпляр 
+ * разработанной структуры. Осуществить использование экземпляра в программе. */
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Task11;
+
+namespace note
+{
+    class structure
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Func func = new Func(k, b);
+            Console.WriteLine(func.Root());
+
+            Console.ReadKey();
+        }
+    }
+}
