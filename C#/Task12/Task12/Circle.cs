1f9845bbee3ce4abc190efe273fdb99f48681b7f
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-20 21:49:19 +0300

    Add files via upload

---
+++ C#/Task12/Task12/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    static class Circle
+    {
+        // метод, определяющий длину окружности по заданному радиусу
+        static public double GetLength(double r)
+        {
+            return Math.Round(2 * Math.PI * r, 3);
+        }
+
+        // метод, определяющий площадь круга по заданному радиусу;
+        static public double GetSquare(double r)
+        {
+            return Math.Round(Math.PI * r * r, 3);
+        }
+
+        // метод, проверяющий принадлежность точки с координатами (x,y)
+        // кругу с радиусом r и координатами центра x0, y0
+        static public string IsinCircle(int x, int y, double r)
+        {
+            double c = Math.Sqrt(x * x + y * y);
+
+            if (c <= r * r) 
+            {
+                return "да";
+            }
+            else
+            {
+                return "нет";
+            }
+        }
+    }
+}
---
+++ C#/Task12/Task12/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Разработать статический класс для работы с окружностью. Класс должен 
+ * содержать 3 метода:
+ *       метод, определяющий длину окружности по заданному радиусу;
+ *       метод, определяющий площадь круга по заданному радиусу;
+ *       метод, проверяющий принадлежность точки с координатами (x,y) кругу с 
+ *              радиусом r и координатами центра x0, y0 */
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Task12;
+
+namespace note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату у: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Площадь круга = {0}", Circle.GetSquare(r));
+            Console.WriteLine("Длина окружности = {0}", Circle.GetLength(r));
+            Console.WriteLine("Принадлежит ли точка с координатами ({0}, {1}) кругу радиусом {2}: {3}", x, y, r, Circle.IsinCircle(x, y, r));
+        }
+    }
+}
