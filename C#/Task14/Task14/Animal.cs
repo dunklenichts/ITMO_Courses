b823260fe07e197b246c7e92eefd058e514b3265
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-21 13:26:52 +0300

    Add files via upload

---
+++ C#/Task14/Task14/Animal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        int isBreed;
+        string name;
+        public override string Name { get => name; set => name = value; }
+
+        public Cat(string name, int isBreed)
+            :base(name)
+        {
+            this.isBreed = isBreed;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+
+    class Dog : Animal
+    {
+        // живет дома или на улице
+        string living;
+        string name;
+        public override string Name { get => name; set => name = value; }
+
+        public Dog(string name, string living)
+            : base(name)
+        {
+            this.living = living;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
