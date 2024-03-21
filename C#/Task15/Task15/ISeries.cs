04f0f10bdb8aca0edd8dc2050d6064d6295c9b23
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-21 15:06:24 +0300

    Add files via upload

---
+++ C#/Task15/Task15/ISeries.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void Reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int start;
+        int current;
+        int step;
+
+        public int getNext()
+        {
+            current += step;
+
+            return current;
+        }
+
+        public void Reset()
+        {
+            current = start;
+        }
+
+        public void setStart(int x)
+        {
+            start = x;
+            current = start;
+        }
+
+        public void setStep(int s) 
+        {
+            step = s;
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        int start;
+        int current;
+        int step;
+
+        public int getNext()
+        {
+            int nextValue = current;
+            current *= step;
+
+            return nextValue;
+        }
+
+        public void Reset()
+        {
+            current = start;
+        }
+
+        public void setStart(int x)
+        {
+            start = x;
+            current = x;
+        }
+
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ C#/Task15/Task15/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Разработать интерфейс ISeries генерации ряда чисел. Интерфейс содержит следующие элементы:
+       - метод void SetStart(int x) - устанавливает начальное значение
+       - метод int GetNext() - возвращает следующее число ряда
+       - метод void Reset() - выполняет сброс к начальному значению
+   
+   Разработать классы ArithProgression и GeomProgression, которые реализуют интерфейс ISeries. 
+   В классах реализовать методы интерфейса в соответствии с логикой арифметической и геометрической 
+   прогрессии соответственно.*/
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Task15;
+
+namespace note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Какую прогрессию использовать: \n1 - арифметическую,\n2 - геометрическую: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Повтор прогрессии: ");
+                int limitNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    ArithProgression arithProgression = new ArithProgression();
+                    arithProgression.setStart(10);
+                    arithProgression.setStep(3);
+
+                    for (int i = 0; i < limitNumber; i++)
+                    {
+                        Console.WriteLine(arithProgression.getNext());
+                    }
+
+                    arithProgression.Reset();
+                }
+                if (choice == 2)
+                {
+                    GeomProgression geomProgression = new GeomProgression();
+                    geomProgression.setStart(10);
+                    geomProgression.setStep(3);
+
+                    for (int i = 0; i < limitNumber; i++)
+                    {
+                        Console.WriteLine(geomProgression.getNext());
+                    }
+
+                    geomProgression.Reset();
+                }
+                if (choice != 1 || choice != 2)
+                {
+                    Console.WriteLine("Выберите 1 или 2");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
