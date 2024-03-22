df4db5895602bf625f5118efb4f4d04c24bce1db
dunklenichts <100723797+dunklenichts@users.noreply.github.com>
2024-03-22 13:45:42 +0300

    Add files via upload

---
+++ C#/Task17/Bank.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Bank <T>
+    {
+        public T AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public string FullName { get; set; }
+
+        public string GetInfo()
+        {
+            return $"Номер счета: {AccountNumber}\nБаланс: {Balance}\nИмя: {FullName}";
+        }
+
+        public void Input()
+        {
+            try
+            {
+                Console.Write("Введите номер счета: ");
+                AccountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+                Console.Write("Введите баланс: ");
+                Balance = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите ФИО: ");
+                FullName = Console.ReadLine();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ C#/Task17/Program2.cs
@@ -0,0 +1,31 @@
+﻿/* Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, 
+ * баланса, ФИО владельца. 
+   Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера 
+   счета. Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. Создать  несколько 
+   экземпляров класса, параметризированного различными типам. Заполнить его поля и вывести на экран 
+   информацию об экземпляре класса. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Bank<int> bank1 = new Bank<int>();
+            bank1.Input();
+            Console.WriteLine(bank1.GetInfo());
+
+            Console.WriteLine();
+
+            Bank<string> bank2 = new Bank<string>();
+            bank2.Input();
+            Console.WriteLine(bank2.GetInfo());
+        }
+    }
+}
